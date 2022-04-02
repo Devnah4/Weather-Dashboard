b4a6ae702558e56f0335cb62dffa04a9f5b49e31
Dev.Nah <mcthundercat99@gmail.com>
2022-04-02 00:42:19 -0400

    Made it pull the data correctly

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,30 +2,21 @@
 
 // Code to search for a city
 function cityEl() {
-    var apiKey = '8e2f43d909354c0af3b25ae5408f8e8c';
-    var lat = document.getElementById('latitude').value;
-    var lon = document.getElementById('longitude').value;
-    var lang = 'en';
-    var units = 'metric';
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    var cityName = document.querySelector('#city-name');
+    let apiKey = '8e2f43d909354c0af3b25ae5408f8e8c';
+    let lang = 'en';
+    let units = 'metric';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey}&lang=${lang}&units=${units}`;
     fetch(apiUrl)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data);
-        // console.log(data.name);
-        // // var temperature = data
-        // for (var i = 0; i <= data.length; ++i) {
-        //     console.log('We in here');
-        //    var cityName = document.createElement('h2') ;
-        //    var weatherUl = document.createElement('ul');
-           
-        //    cityName.textContent = [i].name.value;
-        //    cityInput.appendChild(cityName);
-        })
-    }
-// };
+        namedCity = data.name;
+        
+    })
+ };
 
 
 // Code to display info
